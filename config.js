59d9cc95966d2b8235210089251c63b107208849
giladifrah <116280459+giladifrah@users.noreply.github.com>
2023-12-26 18:08:16 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  botToken: '6330851309:AAHA81MVp-gcl9uH03mJfYjfEBNhRQ0zniA',
+  adminChatId: 1002111890470,       //your telegram chat id here, allows usage of /broadcast command
+}
